f01d7899a91656b70b4c953657e7090d1511aba5
Farah Maqbool <151655153+Farah-Maqbool@users.noreply.github.com>
2026-04-14 05:30:20 +0500

    Add weather tool and integrate into agent
    Introduce a simple weather function and FunctionTool using wttr.in (requests). Add necessary imports (requests, load_dotenv) and register weather_tool with the AgentWorkflow alongside existing tools. Update the example run prompt to request Paris weather. Also clean up/reorder some imports (Groq, dotenv) for clarity.

--- Agentic_Rag/retriever.py
+++ Agentic_Rag/retriever.py
@@ -1,13 +1,15 @@
+import os
+import asyncio
+import requests
 import datasets
+from dotenv import load_dotenv
+from llama_index.llms.groq import Groq
 from llama_index.core.schema import Document
 from llama_index.core.tools import FunctionTool
 from llama_index.retrievers.bm25 import BM25Retriever
 from llama_index.core.agent.workflow import AgentWorkflow
-from llama_index.llms.groq import Groq
 from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
-from dotenv import load_dotenv
-import os
-import asyncio
+
 
 load_dotenv()
 
@@ -53,16 +55,27 @@ search_tool = FunctionTool.from_defaults(
     name="web_search",
     description="Search the web for current info.")
 
+# weather tool
+def get_weather(city: str) -> str:
+    """Get a current weather details for specific city"""
+    response = requests.get(f"https://wttr.in/{city}?format=3")
+    return response.text
+
+weather_tool = FunctionTool.from_defaults(
+    get_weather, 
+    name="get_weather", 
+    description="Get current weather for a city.")
+
 #agent
 llm = Groq(model='openai/gpt-oss-120b', api_key=os.getenv('GROQ_API_KEY'), is_streaming=False)
 
 alferd = AgentWorkflow.from_tools_or_functions(
-    [guest_info_tool, search_tool],
+    [guest_info_tool, search_tool, weather_tool],
     llm=llm,
 )
 
 async def main():
-    response =await alferd.run(user_msg="Who's the current President of France?")
+    response =await alferd.run(user_msg="What's the weather in Paris?")
 
     print(response)
 
